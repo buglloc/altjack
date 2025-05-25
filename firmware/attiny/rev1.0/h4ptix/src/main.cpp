44eb336ae87cd1904c6bc09d964547a4f9bd2011
Andrew Krasichkov <dev@buglloc.cc>
2025-05-25 15:59:24 +0300

    [firmware][rev1.0][h4ptix] minor improvements

--- firmware/attiny/rev1.0/h4ptix/src/main.cpp
+++ firmware/attiny/rev1.0/h4ptix/src/main.cpp
@@ -1,88 +1,104 @@
 #include <Wire.h>
 
-#define I2C_SLAVE_ADDRESS 0x60
-#define SDA_PIN   PIN_PA1
-#define SCL_PIN   PIN_PA2
+#define I2C_SLAVE_ADDR 0x60
+#define SDA_PIN        PIN_PA1
+#define SCL_PIN        PIN_PA2
 
-#define DS_PIN    PIN_PA3  // Data
-#define SHCP_PIN  PIN_PA6  // Shift Clock
-#define STCP_PIN  PIN_PA7  // Latch Clock
+#define SHIFT_DATA_PIN   PIN_PA3
+#define SHIFT_CLOCK_PIN  PIN_PA6
+#define SHIFT_LATCH_PIN  PIN_PA7
 
-#define PORT_CNT  4
+#define NUM_PORTS  4
 
-volatile uint8_t shiftState = 0;
-static uint32_t portTimers[PORT_CNT] = {0};
-static bool portActive[PORT_CNT] = {false};
-static uint8_t portBits[PORT_CNT] = {
-  0x01,
-  0x02,
-  0x06,
-  0x07,
+typedef struct  {
+  bool active;
+  uint32_t until;
+  uint8_t outBit;
+} port;
+
+port ports[NUM_PORTS] = {
+  {false, 0, 0x01},
+  {false, 0, 0x02},
+  {false, 0, 0x06},
+  {false, 0, 0x07},
 };
 
-uint8_t calcState() {
-  uint8_t state = 0;
-  for (uint8_t i = 0; i < PORT_CNT; i++) {
-    if (portActive[i]) {
-      state |= (1 << portBits[i]);
-    }
-  }
-
-  return state;
-}
-
-void shiftOutState(uint8_t value) {
+void writeShiftRegister(uint8_t value) {
   // MSB first, shift out
   for (int i = 7; i >= 0; i--) {
-    digitalWrite(SHCP_PIN, LOW);
-    digitalWrite(DS_PIN, (value >> i) & 0x01);
-    digitalWrite(SHCP_PIN, HIGH);
+    digitalWrite(SHIFT_CLOCK_PIN, LOW);
+    digitalWrite(SHIFT_DATA_PIN, (value >> i) & 0x01);
+    digitalWrite(SHIFT_CLOCK_PIN, HIGH);
   }
 
   // Latch
-  digitalWrite(STCP_PIN, LOW);
-  digitalWrite(STCP_PIN, HIGH);
+  digitalWrite(SHIFT_LATCH_PIN, LOW);
+  digitalWrite(SHIFT_LATCH_PIN, HIGH);
 }
 
-void shiftStateUpdate() {
-  shiftState = calcState();
-  shiftOutState(shiftState);
+uint8_t calcRegister() {
+  uint8_t out = 0;
+  for (uint8_t i = 0; i < NUM_PORTS; i++) {
+    if (ports[i].active) {
+      out |= (1 << ports[i].outBit);
+    }
+  }
+
+  return out;
+}
+
+void syncRegister() {
+  writeShiftRegister(calcRegister());
 }
 
 void onReceive(int len) {
-  if (len < 3) {
+  uint8_t buf[3] = {0};
+
+  if (len < sizeof(buf)) {
     return;
   }
 
-  uint8_t port = Wire.read();
-  if (port >= PORT_CNT) {
+  uint8_t bufIdx = 0;
+  while (Wire.available() && bufIdx < sizeof(buf)) {
+    buf[bufIdx++] = Wire.read();
+    len--;
+  }
+
+  // read garbage if any
+  while (Wire.available() && len > 0) {
+    Wire.read();
+    len--;
+  }
+
+
+  uint8_t port = buf[0];
+  if (port >= NUM_PORTS) {
     return;
   }
 
-  uint16_t duration = Wire.read();  // low byte
-  duration |= (Wire.read() << 8);  // high byte
+  uint16_t duration = (uint16_t)buf[1] | ((uint16_t)buf[2] << 8);
 
-  portActive[port] = true;
-  portTimers[port] = millis() + duration;
-
-  shiftStateUpdate();
+  ports[port].active = true;
+  ports[port].until = millis() + duration;
+  syncRegister();
 }
 
 void onRequest() {
-  Wire.write(calcState()); // Echo back the last value
+  // Echo back the last value
+  Wire.write(calcRegister());
 }
 
 void setup() {
   // Shift register
-  pinMode(DS_PIN, OUTPUT);
-  pinMode(SHCP_PIN, OUTPUT);
-  pinMode(STCP_PIN, OUTPUT);
+  pinMode(SHIFT_DATA_PIN, OUTPUT);
+  pinMode(SHIFT_CLOCK_PIN, OUTPUT);
+  pinMode(SHIFT_LATCH_PIN, OUTPUT);
 
-  shiftOutState(0x00);
+  syncRegister();
 
   // I2C
   Wire.pins(SDA_PIN, SCL_PIN);
-  Wire.begin(I2C_SLAVE_ADDRESS);
+  Wire.begin(I2C_SLAVE_ADDR);
   Wire.onReceive(onReceive);
   Wire.onRequest(onRequest);
 }
@@ -91,14 +107,14 @@ void loop() {
   uint32_t now = millis();
   bool updated = false;
 
-  for (int i = 0; i < PORT_CNT; i++) {
-    if (portActive[i] && now >= portTimers[i]) {
-      portActive[i] = false;
+  for (int i = 0; i < NUM_PORTS; i++) {
+    if (ports[i].active && now >= ports[i].until) {
+      ports[i].active = false;
       updated = true;
     }
   }
 
   if (updated) {
-    shiftStateUpdate();
+    syncRegister();
   }
 }
